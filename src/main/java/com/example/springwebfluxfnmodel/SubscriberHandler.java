9c0c056e9ac530ce7415de854bf8057c793aabc6
Tetsuji ITAGAKI <t-itagaki@atware.co.jp>
2022-05-13 16:40:38 +0900

    不要な変数を除去

--- src/main/java/com/example/springwebfluxfnmodel/SubscriberHandler.java
+++ src/main/java/com/example/springwebfluxfnmodel/SubscriberHandler.java
@@ -2,18 +2,15 @@ package com.example.springwebfluxfnmodel;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
 public class SubscriberHandler {
-    private final WebClient client;
     private final Subscriber subscriber;
 
-    public SubscriberHandler(WebClient.Builder builder, Subscriber subscriber) {
-        this.client = builder.baseUrl("http://localhost:8080").build();
+    public SubscriberHandler(Subscriber subscriber) {
         this.subscriber = subscriber;
     }
 
